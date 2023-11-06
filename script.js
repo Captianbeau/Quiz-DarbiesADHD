639624f1752505af3d3b5eeb41a6555d74c7d99f
Capti <captianbeau@gmail.com>
2023-11-06 13:42:56 -0700

    added code

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // needs timer, event listeners, prevents-default/propagation, data displays, content, style
 //timer for each question?
+
+//  
+var ques1 ={
+    question:"When default what does “this” refer to?",
+    A: "The Parent",
+    B: "First Function",
+    C:"The Global Object",
+    D: "The Windshield",
+    // correct answer C
+    quiz: function(){
+        
+        if (  ){
+
+        }else{
+
+        }
+    }
+}
