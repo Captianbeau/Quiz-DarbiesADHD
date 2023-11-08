672a16f00d2afa72dffe492f511f70bcac5e8ce4
Capti <captianbeau@gmail.com>
2023-11-07 22:14:59 -0700

    added code

--- script.js
+++ script.js
@@ -2,19 +2,52 @@
 //timer for each question?
 
 //  
+var question = (".question")
+var answers = (".answers")
+var scoreLocation = (".score")
+var score = [];
 var ques1 ={
-    question:"When default what does “this” refer to?",
-    A: "The Parent",
-    B: "First Function",
-    C:"The Global Object",
-    D: "The Windshield",
-    // correct answer C
-    quiz: function(){
-        
-        if (  ){
+    question:"What can stop cats from producing allergens?",
+    A:"Egg yolks from a chicken raised with cats",
+    B:"Making cats go vegan",
+    C:"The most expensive angus beef",
+    D:"Shaving their hair",
+    correct:"A"
+    // correct answer A
+}
+var ques2 ={
+    question:"What pigment is extremely rare to find in nature?",
+    A:"Purple",
+    B:"Red",
+    C:"Blue",
+    D:"Yellow",
+    correct:"C"
 
-        }else{
-
-        }
-    }
+}
+var ques3 ={
+    question:"Why is a tiger orange?",
+    A:"They're fancy",
+    B:"Their prey is color blind",
+    C:"To show they are dangerous",
+    D:"They are brown",
+    correct:"B"
+}
+var ques4 ={
+    question:"What are daddy-long-legs?",
+    A:"The most venomous spider in the world",
+    B:"Scorpions and they aren't venomous at all",
+    C:"The second most venomous spider in the world",
+    D:"A delightful snack",
+    correct:"B"
+}
+var ques5 ={
+    question:"Why were bugs so big in the Carboniferous period?",
+    A:"They had no predators so they could grow",
+    B:"There weren't any animals to breathe so there was more oxygen",
+    C:"Plants produced more oxygen because the ozone layer hadn't developed",
+    D:"Trees couldn't decay so it trapped carbon and produced an abundance of oxygen ",
+    correct:"D"
+}
+quiz = function(){
+    
 }
