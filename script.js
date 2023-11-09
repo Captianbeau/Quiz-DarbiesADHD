789102b07f34d409339f5303441c13c8cf35ba05
Capti <captianbeau@gmail.com>
2023-11-09 11:27:28 -0700

    java code added

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 //timer for each question?
 
 //  
-var header = document.querySelector("header")
-var startbttn = document.querySelector("button")
+var header = document.querySelector(".header")
+var startButton = document.querySelector("#start")
 var questionSpace = document.querySelector(".question")
 var answers = document.querySelector(".answers")
 var scoreLocation = document.querySelector(".score")
+var retryButton = document.querySelector(".retry")
+
+
 var score = [];
 var ques1 ={
     question:"What can stop cats from producing allergens?",
@@ -14,25 +17,20 @@ var ques1 ={
     B:"Making cats go vegan",
     C:"The most expensive angus beef",
     D:"Shaving their hair",
-    correct: A
+    // correct: ques1.A
     // correct answer A
 }
 var ques2 ={
     question:"What pigment is extremely rare to find in nature?",
-    A:"Purple",
-    B:"Red",
-    C:"Blue",
-    D:"Yellow",
-    correct: C
+    answers:["Purple","Red","Blue","Yellow"]
+   
+    // correct: ques2.C
 
 }
 var ques3 ={
     question:"Why is a tiger orange?",
-    A:"They're fancy",
-    B:"Their prey is color blind",
-    C:"To show they are dangerous",
-    D:"They are brown",
-    correct: B
+    answers:["They're fancy","Their prey is color blind","To show they are dangerous","They are brown"]
+    // correct: ques3.B
 }
 var ques4 ={
     question:"What are daddy-long-legs?",
@@ -40,7 +38,7 @@ var ques4 ={
     B:"Scorpions and they aren't venomous at all",
     C:"The second most venomous spider in the world",
     D:"A delightful snack",
-    correct: B
+    // correct: ques4.B
 }
 var ques5 ={
     question:"Why were bugs so big in the Carboniferous period?",
@@ -48,19 +46,32 @@ var ques5 ={
     B:"There weren't any animals to breathe so there was more oxygen",
     C:"Plants produced more oxygen because the ozone layer hadn't developed",
     D:"Trees couldn't decay so it trapped carbon and produced an abundance of oxygen ",
-    correct: D
+    // correct: ques5.D
 }
-quiz = function(event){
-//hide header and pullup first question
-//    header.setAttribute('style','display:none')
-var ques = document.createElement('h2')
-ques.textContent(ques1['question'])
-questionSpace.appendChild(ques)
+
+
+function quiz(event){
+//hide header and display first question
+// event.preventDefault
+header.setAttribute("style","display:none");
+questionSpace.setAttribute("style","display:flex");
+answers.setAttribute("style","display:block");
+
 
 }
-quesChange = function(){
+function ques2Change(event){
 //go through questions and display if right or wrong
-if ()
+if(questionSpace.children[0] == ques1.question){
+questionSpace.children[0].textContent = ques2.question;
+for( i = 0 ; i <= 4 ; i++){
+answers.children[i].textContent = ques2.answers[i];
+};
+ } else if(questionSpace.children[0] == ques3.question)
+ questionSpace.children[0].textContent = ques3.question;
+// for( i = 0 ; i <= 4; i++){
+// answers.children[i].textContent = ques3.answers[i];
+// }
 }
-startbttn.addEventListener('click',quiz())
+startButton.addEventListener("click", quiz);
+answers.addEventListener("click", ques2Change);
 // add a function that calculates and displays score also more event listeners for answers and replay
